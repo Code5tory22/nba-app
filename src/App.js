bd7e12bc8ca3511fdfc96af72259a668a8f1a0e5
brandon gibson <you@example.com>
2023-12-08 02:01:40 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [players, setPlayers] = useState("");
+
+  async function NBAData() {
+    const url = "https://api-nba-v1.p.rapidapi.com/players?team=1&season=2021";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "1158b94046msh17179fdd8e7c662p131ecejsn1be4707ebfaf",
+        "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
+      },
+    };
+    const response = await fetch(url, options);
+    const result = await response.json();
+    setPlayers(result.response);
+  }
+
+  useEffect(() => {
+    NBAData();
+  }, []);
+
+  console.log(players)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <button onClick={NBAData}>Search</button>
+      {players.map((player) => {
+        return (
+          <ul>
+            <li>{player.firstname}</li>
+          </ul>
+        )
+      } )}
     </div>
   );
 }
